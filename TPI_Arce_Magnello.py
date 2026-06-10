07e0ec767409faded657717d1576e8d5593258d1
oliverio97 <oliverio.arce97@gmail.com>
2026-06-10 19:36:32 -0300

    Creacion estructuras principales

---
+++ TPI_Arce_Magnello.py
@@ -0,0 +1,53 @@
+import csv
+
+### FUNCIONES PRINCIPALES ###
+
+
+# AGREGAR PAIS
+def agregar_pais():
+    pass
+
+
+# ACTUALIZAR POBLACION Y SUPERFICIE PAIS
+def actualizar_pob_sup():
+    pass
+
+
+# BUSCAR PAIS POR NOMBRE
+def buscar_pais():
+    pass
+
+
+# FILTRAR PAISES (VER SI HACEMOS UNA SOLA O VARIAS)
+def filtrar_pais():
+    pass
+
+
+# ORDENAR PAISES POR NOMBRE, POBLACION, SUPERFICIE
+def ordenar_pais():
+    pass
+
+
+# MOSTRAR ESTADISTICAS:
+def mostrar_estadisticas():
+    pass
+
+
+# BLOQUE PRINCIPAL
+
+while True:  # aca poner el menu con sus validaciones
+    menu = ""
+    pass
+
+    match menu:
+        case 1:
+            pass
+
+        case 2:
+            pass
+
+        case 3:
+            pass
+
+        case _:
+            pass
